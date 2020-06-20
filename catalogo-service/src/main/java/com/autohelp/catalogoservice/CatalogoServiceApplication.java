fb0c62991f6382811208ff235e8053e1eed36d8b
SidneiReis <sidnei.reis@gmail.com>
2020-06-20 18:43:27 -0300

    Versão Avaliação

--- catalogo-service/src/main/java/com/autohelp/catalogoservice/CatalogoServiceApplication.java
+++ catalogo-service/src/main/java/com/autohelp/catalogoservice/CatalogoServiceApplication.java
@@ -3,7 +3,9 @@ package com.autohelp.catalogoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
 public class CatalogoServiceApplication {
